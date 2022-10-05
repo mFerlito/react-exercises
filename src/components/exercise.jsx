666380e197dce8a148173b77a3500e3de12a4ff9
Marco Ferlito <MarcoFerlito95@hotmail.com>
2022-10-05 17:45:21 +0200

    components-06 done

--- src/components/exercise.jsx
+++ src/components/exercise.jsx
@@ -5,6 +5,7 @@ export class Hello extends React.Component {
     render() {
         return <div> 
             <h1>Hello, World!!!</h1>
+            <p>What a beautiful day!</p>
         </div>
     }
 }
