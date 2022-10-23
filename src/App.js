7630270cbd6efe9a0065d6e3d2edd47111f05409
Marco Ferlito <MarcoFerlito95@hotmail.com>
2022-10-23 21:07:11 +0200

    props-02 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { Hello } from "./components/exercise"
+import React from 'react'
+import { Hello } from './components/Hello'
 import { Welcome } from './components/Welcome'
 
 export class App extends React.Component {
-  render() {
-      return <div>
-          <Hello />
-          <Welcome name= "Marco" />
-      </div>
-  }
+    render() {
+        return <div>
+            <Hello />
+            <Welcome name= "Marco" age="26" />
+        </div>
+        
+    }
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,6 +4,9 @@ import React from "react";
 
 export class Welcome extends React.Component {
     render () {
-        return <h2>Welcome, {this.props.name} {this.props.surname}</h2>
+        return <div>
+        <h2>Welcome, {this.props.name ?? 'Andrea'}</h2>
+        <p>Your Age is {this.props.age}</p>
+</div>
     }
 }
