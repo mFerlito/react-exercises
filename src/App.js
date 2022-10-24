0d90db9bccd4130dbc24b4dbe0dc8604f5d07774
Marco Ferlito <MarcoFerlito95@hotmail.com>
2022-10-24 09:21:03 +0200

    conditional-rendering-05 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import { Age } from './components/Age' 
 import { Hello } from './components/Hello'
 import { Welcome } from './components/Welcome'
 
 export class App extends React.Component {
     render() {
         return <div>
-            <Hello />
-            <Welcome name={ 'Marco' } age={26} />
+            <Age age={18} /> 
         </div>
         
     }
