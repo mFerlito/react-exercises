25348e5688f33be70e8818ac9e40126588166090
Marco Ferlito <MarcoFerlito95@hotmail.com>
2022-10-24 09:34:27 +0200

    state-01 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Age } from './components/Age' 
+import {Counter} from './components/Counter'
 import { Hello } from './components/Hello'
 import { Welcome } from './components/Welcome'
 
 export class App extends React.Component {
     render() {
         return <div>
-            <Age age={18} /> 
+            <Age /> 
+            <Counter /> 
         </div>
         
     }
