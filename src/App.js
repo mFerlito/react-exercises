169e3299634760a41840b6dfa0018b5b711b39ec
Marco Ferlito <MarcoFerlito95@hotmail.com>
2022-10-24 14:19:22 +0200

    forms-01 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { ClickTracker } from './components/ClickTracker'
 import { Counter } from './components/Counter'
+import { InteractiveWelcome } from './components/InteractiveWelcome'
 
 
 export class App extends React.Component {
     render() {
         return <div>
-            
+<InteractiveWelcome /> 
             <Counter initialValue={0} incrementInterval={1000} incrementAmount ={1}/>
             <ClickTracker />
         </div>
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,7 +6,7 @@ import { Age } from "./Age";
 export class Welcome extends React.Component {
     render () {
         return <div>
-        <h2>Welcome,{this.props.name ?? 'Marco'}</h2>
+        <h2>Welcome, {this.props.name ?? 'Marco'}</h2>
         {/*  {this.props.age > 18 && this.props.age < 65 && this.props.name === "John" && <Age age={this.props.age}  />} */}
 </div>
     }
