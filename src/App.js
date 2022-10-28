acfe7b1e7c31fc5c3ea470c1c8eed7f0fa43a7fe
Marco Ferlito <MarcoFerlito95@hotmail.com>
2022-10-28 11:28:06 +0200

    Function-components-01 done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Welcome } from './components/Welcome'
-import { Container } from './components/Container'
 import { DisplayLanguage } from './components/DisplayLanguage'
+import { Welcome } from './components/Welcome'
+
+
+
+
 
 export class App extends React.Component {
     render() {
         return <div>
-        
-            <Container title={'This is an awesome title passed as prop :D'} />
-            <DisplayLanguage /> 
-            
+
+
+            <Welcome />  
+
         </div>
 
     }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,14 +1,11 @@
-
 import React from "react";
-import './index.css';
-import { Age } from "./Age";
+ 
 
-
-export class Welcome extends React.Component {
-    render () {
-        return <div className="welcome">
-        <h2>Welcome, {this.props.name ?? 'Marco'}</h2>
-        {/*  {this.props.age > 18 && this.props.age < 65 && this.props.name === "John" && <Age age={this.props.age}  />} */}
+export function Welcome () {
+    
+        return <div className="welcome">  
+        <h2>Welcome, {Welcome.name && "Marco" }</h2> 
+        
 </div>
-    }
+    
 }
