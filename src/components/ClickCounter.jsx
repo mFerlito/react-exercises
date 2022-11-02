0468db326f560d0ddf8f4cb6c2da09809bd360b9
Marco Ferlito <MarcoFerlito95@hotmail.com>
2022-11-02 17:20:40 +0100

    useeffect-01 done

--- src/components/ClickCounter.jsx
+++ src/components/ClickCounter.jsx
@@ -1,16 +1,34 @@
-import { useState, React } from "react";
+import { useEffect, useState, React } from "react";
+
+
 
 export function ClickCounter({ initialValue = 0 }) {
     const [counter, setCounter] = useState(initialValue)
+
+    function handleCounterDecrement() {
+        setCounter(c => c - 1)
+    }
+    function handleCounterReset() {
+        setCounter(initialValue)
+    }
+    function onCounterChange(value) {
+        console.log(value);
+    }
     function handleCounterIncrement() {
         setCounter(c => c + 1)
     }
+    useEffect(()=>{
+        onCounterChange(counter)
+    },[counter])
+    
 
     return (
         <div>
 
             <h3>Count: {counter}</h3>
             <button onClick={handleCounterIncrement}>Click to increment</button>
+            <button onClick={handleCounterDecrement}>Click to decrement</button>
+            <button onClick={handleCounterReset}>Click to Reset</button> 
         </div>
     )
 
