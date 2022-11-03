5429ed24c7a6ed09d9e8f51d21fcd741157bcd3f
Marco Ferlito <MarcoFerlito95@hotmail.com>
2022-11-03 13:17:43 +0100

    react-router-03 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import { Welcome } from "./components/Welcome";
 
+import { ShowGithubUser } from "./components/ShowGithubUser";
+
 export function App() {
-    
+
     return (
         <Routes>
-            <Route path="/" element={<Welcome name='Marco'/>} />
+         
+            
+            <Route path="/user/:username" element={<ShowGithubUser />} />
         </Routes>
     )
 
